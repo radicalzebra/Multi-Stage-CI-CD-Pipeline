d8f80aeaa144461a9d899fa59cc357d995402cce
Arshad <ourself786@gmail.com>
2025-10-02 20:15:48 +0530

    Left trivy usage for now

--- Jenkins/scripts/pushImage.sh
+++ Jenkins/scripts/pushImage.sh
@@ -3,31 +3,15 @@
 set -e #ensures the script exits immediately on non-zero return codes.
 
 
-# Trivy scan built image
-docker run --rm \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v /var/jenkins_home/trivy-cache:/root/.cache/ \
-  -v /var/jenkins_home/tmp:/tmp \
-  aquasec/trivy:latest image \
-  --severity HIGH,CRITICAL \
-  --exit-code 1 \
-  "$DOCKER_USER/bankist:latest"
-
-
-
-
-#Full Trivy report (JSON, saved for logs)
-mkdir -p trivy-reports
-docker run --rm \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v /var/jenkins_home/trivy-cache:/root/.cache/ \
-  -v /var/jenkins_home/tmp:/tmp \
-  aquasec/trivy:latest image \
-  --format json \
-  --output trivy-reports/full-scan.json \
-  "$DOCKER_USER/bankist:latest"
-
-
+# Left trivy due to issue with windows annd temporary filesystem size issue
+# docker run --rm \
+#   -v /var/run/docker.sock:/var/run/docker.sock \
+#   -v /var/jenkins_home/trivy-cache:/root/.cache/ \
+#   -v /var/jenkins_home/tmp:/tmp \
+#   aquasec/trivy:latest image \
+#   --severity HIGH,CRITICAL \
+#   --exit-code 1 \
+#   "$DOCKER_USER/bankist:latest"
 
 
 #If scan is successfull then push to docker hub
